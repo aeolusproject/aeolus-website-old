ebfb5930a8f16c2a6bb7a82d483756c434dea293
Tomas Sedovic <tsedovic@redhat.com>
2010-06-23 18:05:40 +0200

    Rearrange the order of the documetation pages.

--- deltacloud.org/lib/documentation.rb
+++ deltacloud.org/lib/documentation.rb
@@ -4,9 +4,9 @@ module DocumentationHelper
       { :href => 'documentation.html', :menu => 'Overview' },
       { :href => "api.html" , :menu => 'REST API', :description => 'REST API definition' },
       { :href => "drivers.html" , :menu => 'Drivers', :description => 'Currently-supported drivers' },
-      { :href => "image-builder.html" , :menu => 'Image Builder', :description => 'Image Builder' },
       { :href => "framework.html" , :menu => 'Framework', :description => 'Framework for writing additional drivers' },
       { :href => "client-ruby.html" , :menu => 'Ruby Client', :description => 'Ruby client' },
+      { :href => "image-builder.html" , :menu => 'Image Builder', :description => 'Image Builder' },
     ]
   end
 
