a8d595410737345af85c50a913d36c8d1ae0561f
Tomas Sedovic <tsedovic@redhat.com>
2010-06-23 13:46:51 +0200

    Use single list of documentation pages.
    Previously, we used a separate list of the doc pages for the menu (in
    deltacloud.org/content/_submenu_documentation.haml) and for the documentation
    Overview page (in docs/documentation.mdown).
    
    This fix adds a single source located in deltacloud.org/lib/documentation.rb
    which is used both for the menu and the overview page. It makes
    adding/renaming/moving the doc pages easier.

---
+++ deltacloud.org/lib/documentation.rb
@@ -0,0 +1,15 @@
+module DocumentationHelper
+  def documentation_pages
+    [
+      { :href => 'documentation.html', :menu => 'Overview' },
+      { :href => "api.html" , :menu => 'REST API', :description => 'REST API definition' },
+      { :href => "drivers.html" , :menu => 'Drivers', :description => 'Currently-supported drivers' },
+      { :href => "image-builder.html" , :menu => 'Image Builder', :description => 'Image Builder' },
+      { :href => "framework.html" , :menu => 'Framework', :description => 'Framework for writing additional drivers' },
+      { :href => "client-ruby.html" , :menu => 'Ruby Client', :description => 'Ruby client' },
+    ]
+  end
+
+end
+
+Webby::Helpers.register(DocumentationHelper)
