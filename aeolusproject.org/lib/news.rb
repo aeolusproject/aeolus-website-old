172ef6112a46bfae010d4da42ae9dba8510b923a
Chris Lalancette <clalance@redhat.com>
2011-01-18 10:11:48 -0500

    A number of updates to clean the site up a bit.
    Signed-off-by: Chris Lalancette <clalance@redhat.com>

--- aeolusproject.org/lib/news.rb
+++ aeolusproject.org/lib/news.rb
@@ -1,7 +1,7 @@
 module NewsHelper
   def all_news
     [
-      %Q(Packages for Aeolus Conductor are now available for Fedora 13, Fedora 14, and RHEL-6.  See http://repos.fedorapeople.org/repos/deltacloud/appliance/),
+      %Q(Packages for Aeolus Conductor are now available for Fedora 13, Fedora 14, and RHEL-6.  Click <a href="http://repos.fedorapeople.org/repos/aeolus/packages/">here</a> to get to the repositories.),
       %Q(Deltacloud Aggregator renamed to Aeolus Project),
     ]
   end
