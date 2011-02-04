750c8f475801e0ea8025178163fdc680dac516ff
Chris Lalancette <clalance@redhat.com>
2011-02-04 11:22:54 -0500

    Significantly expand the documentation for Oz.
    Signed-off-by: Chris Lalancette <clalance@redhat.com>

--- aeolusproject.org/lib/oz.rb
+++ aeolusproject.org/lib/oz.rb
@@ -2,7 +2,14 @@ module OzHelper
   def oz_pages
     [
       { :href => 'oz.html', :menu => 'Overview' },
+      { :href => 'oz-download.html', :menu => "Downloads", :description => "Downloads" },
+      { :href => 'oz-examples.html', :menu => "Examples", :description => "Examples" },
+      { :href => 'oz-architecture.html', :menu => "Architecture", :description => "Architecture" },
+      { :href => 'oz-advanced.html', :menu => "Advanced", :description => "Advanced usage" },
       { :href => 'oz-contribute.html', :menu => "Contribute", :description => "Contribute to oz development" },
+      { :href => 'oz-install.html', :menu => "oz-install", :description => "oz-install man page" },
+      { :href => 'oz-customize.html', :menu => "oz-customize", :description => "oz-customize man page" },
+      { :href => 'oz-generate-icicle.html', :menu => "oz-generate-icicle", :description => "oz-generate-icicle man page" },
     ]
   end
 
---
+++ aeolusproject.org/lib/oz_support.rb
@@ -0,0 +1,27 @@
+module OzSupportHelper
+  def ozguests
+    [
+     { :name => "Fedora 7,8,9,10,11,12,13,14", :install => true, :customize => true, :icicle => true },
+     { :name => "FedoraCore 1,2,3,4,5,6", :install => true, :customize => false, :icicle => false },
+     { :name => "RHEL-6", :install => true, :customize => true, :icicle => true },
+     { :name => "RHEL-5.{0,1,2,3,4,5,6}", :install => true, :customize => true, :icicle => true },
+     { :name => "RHEL-4.{0,1,2,3,4,5,6,7,8}", :install => true, :customize => false, :icicle => false },
+     { :name => "RHEL-3.{0,1,2,3,4,5,6,7,8,9}", :install => true, :customize => false, :icicle => false },
+     { :name => "RHEL-2.1 GOLD, U1, U2, U3, U4, U5, U6", :install => true, :customize => false, :icicle => false },
+     { :name => "OpenSUSE 11.{1,2,3}", :install => true, :customize => false, :icicle => false },
+     { :name => "Ubuntu 6.10, 7.04, 7.10, 8.04, 8.10, 9.04, 9.10", :install => true, :customize => false, :icicle => false },
+     { :name => "Windows XP, 2003, 7, 2008", :install => true, :customize => false, :icicle => false },
+     { :name => "Red Hat Linux 7.0, 7.1, 7.2, 7.3, 8, 9", :install => true, :customize => false },
+    ]
+  end
+
+  LABELS = { true => "yes", false => "no" }
+
+  def support_indicator(value)
+    text = LABELS[value] || "TBD"
+    cls = value ? "supported" : "not-supported"
+    "<td class=\"#{cls}\">#{text}</td>"
+  end
+end
+
+Webby::Helpers.register(OzSupportHelper)
