1fe86ebf082b256643c060319eba740ea97a0d32
Chris Lalancette <clalance@redhat.com>
2011-03-23 13:45:09 -0400

    Add a page about optimizing Oz performance.
    Signed-off-by: Chris Lalancette <clalance@redhat.com>

--- aeolusproject.org/lib/oz.rb
+++ aeolusproject.org/lib/oz.rb
@@ -4,6 +4,7 @@ module OzHelper
       { :href => 'oz.html', :menu => 'Overview' },
       { :href => 'oz-download.html', :menu => "Downloads", :description => "Downloads" },
       { :href => 'oz-examples.html', :menu => "Examples", :description => "Examples" },
+      { :href => 'oz-performance.html', :menu => "Performance", :description => "Performance" },
       { :href => 'oz-architecture.html', :menu => "Architecture", :description => "Architecture" },
       { :href => 'oz-advanced.html', :menu => "Advanced", :description => "Advanced usage" },
       { :href => 'oz-contribute.html', :menu => "Contribute", :description => "Contribute to oz development" },
