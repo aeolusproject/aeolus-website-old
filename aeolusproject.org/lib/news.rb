6ba0bf38a87e7e20b3f396dff1c7fdfeabc8b111
Chris Lalancette <clalance@redhat.com>
2011-04-14 08:45:02 -0400

    Add the release of Oz 0.3.0 to the News.
    Signed-off-by: Chris Lalancette <clalance@redhat.com>

--- aeolusproject.org/lib/news.rb
+++ aeolusproject.org/lib/news.rb
@@ -1,6 +1,7 @@
 module NewsHelper
   def all_news
     [
+      %Q(Oz version 0.3.0 is released.  Tarballs and packages are available from <a href="oz-download.html">here</a>.),
       %Q(Oz version 0.2.0 is released.  Tarballs and packages are available from <a href="oz-download.html">here</a>.),
       %Q(Oz version 0.1.0 is released.  Tarballs and packages are available from <a href="oz-download.html">here</a>.),
       %Q(Packages for Aeolus Conductor are now available for Fedora 13, Fedora 14, and RHEL-6.  Click <a href="http://repos.fedorapeople.org/repos/aeolus/packages/">here</a> to get to the repositories.),
