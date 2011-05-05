11a54a096ac32b7483cfba4a1caad1a2c723909f
Jakub Steiner <jimmac@gmail.com>
2011-05-05 23:47:58 +0200

    fix paths, correct dealing with fixed positioned footer on resize.

--- aeolusproject.org/content/js/aeolus.js
+++ aeolusproject.org/content/js/aeolus.js
@@ -1,5 +1,6 @@
 function positionFooter() {
   var $footer = $('#footer');
+  $footer.removeClass('fixed');
   if ($(document.body).height() < $(window).height()) {
     $footer.addClass('fixed');
   } else {
@@ -7,6 +8,23 @@ function positionFooter() {
   }
 }
 
+function preventFOUT() {
+  // if firefox 3.5+, hide content till load (or 3 seconds) to prevent FOUT
+  var d = document, e = d.documentElement, s = d.createElement('style');
+  if (e.style.MozTransform === ''){ // gecko 1.9.1 inference
+    s.textContent = 'body{visibility:hidden}';
+    var r = document.getElementsByTagName('script')[0];
+    r.parentNode.insertBefore(s, r);
+    function f(){ s.parentNode && s.parentNode.removeChild(s); }
+    addEventListener('load',f,false);
+    setTimeout(f,3000); 
+  }
+}
+
 $(document).ready(function () {
-  $(window).scroll(positionFooter).resize(positionFooter).scroll();
+  $(window).scroll(positionFooter)
+        .resize(positionFooter).scroll();
+   //preventFOUT();
 });
+
+
