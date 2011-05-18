f6149b21a65642f1b322a78e1a6802c5b11be735
Justin Clift <jclift@redhat.com>
2011-05-19 06:12:40 +1000

    Added visual distinction between fully and partially supported guests, with info.

--- aeolusproject.org/lib/oz_support.rb
+++ aeolusproject.org/lib/oz_support.rb
@@ -1,23 +1,27 @@
 module OzSupportHelper
-  def ozguests
+  def ozguestsfull
     [
-     { :name => "Fedora 7,8,9,10,11,12,13,14", :install => true, :customize => true, :icicle => true },
-     { :name => "FedoraCore 1,2,3,4,5,6", :install => true, :customize => false, :icicle => false },
-     { :name => "RHEL-6", :install => true, :customize => true, :icicle => true },
-     { :name => "RHEL-5.{0,1,2,3,4,5,6}", :install => true, :customize => true, :icicle => true },
-     { :name => "RHEL-4.{0,1,2,3,4,5,6,7,8}", :install => true, :customize => false, :icicle => false },
-     { :name => "RHEL-3.{0,1,2,3,4,5,6,7,8,9}", :install => true, :customize => false, :icicle => false },
-     { :name => "RHEL-2.1 GOLD, U1, U2, U3, U4, U5, U6", :install => true, :customize => false, :icicle => false },
-     { :name => "OpenSUSE 11.{0,1,2,3,4}", :install => true, :customize => false, :icicle => true },
-     { :name => "Ubuntu 6.10, 7.04, 7.10, 8.04, 8.10, <br /> 9.04, 9.10, 10.04, 10.10", :install => true, :customize => false, :icicle => false },
-     { :name => "Windows 2000, XP, 2003, 7, 2008", :install => true, :customize => false, :icicle => false },
-     { :name => "Red Hat Linux 7.0, 7.1, 7.2, 7.3, 8, 9", :install => true, :customize => false },
-     { :name => "CentOS 3.{0,1,2,3,4,5,6,7,8,9}", :install => true, :customize => false, :icicle => false },
-     { :name => "CentOS 4.{0,1,2,3,4,5,6,7,8,9}", :install => true, :customize => false, :icicle => false },
-     { :name => "CentOS 5.{0,1,2,3,4,5,6}", :install => true, :customize => true, :icicle => true },
-     { :name => "Debian 5, 6", :install => true, :customize => false, :icicle => false },
+     { :name => "CentOS 5.x", :install => true, :customize => true, :icicle => true },
+     { :name => "Fedora 7 - 14", :install => true, :customize => true, :icicle => true },
+     # Had to be moved out of this array, and into the content page itself, so it could be visually styled correctly 
+     #{ :name => "RHEL 5.x, 6", :install => true, :customize => true, :icicle => true },
     ]
   end
+
+  def ozguestspartial
+    [
+     { :name => "CentOS 3.x, 4.x", :install => true, :customize => false, :icicle => false },
+     { :name => "Debian 5, 6", :install => true, :customize => false, :icicle => false },
+     { :name => "Fedora Core 1 - 6", :install => true, :customize => false, :icicle => false },
+     { :name => "OpenSUSE 11.x", :install => true, :customize => false, :icicle => true },
+     { :name => "Red Hat Linux 7 - 9", :install => true, :customize => false },
+     { :name => "RHEL 2.x, 3.x, 4.x <br /><em>(4.9 not fully tested)</em>", :install => true, :customize => false, :icicle => false },
+     { :name => "Ubuntu 6.10 - 10.10", :install => true, :customize => false, :icicle => false },
+     # Had to be moved out of this array, and into the content page itself, so it could be visually styled correctly
+     #{ :name => "Windows 2000, XP, 2003, 7, 2008", :install => true, :customize => false, :icicle => false },
+    ]
+  end
+
 
   LABELS = { true => "yes", false => "no" }
 
