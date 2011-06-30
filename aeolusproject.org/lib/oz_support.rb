4a3889a6546ad1fa9a2feae2660292f45e0c9b46
Chris Lalancette <clalance@redhat.com>
2011-06-30 17:50:42 -0400

    Updates to the website for the Oz 0.5.0 release.
    Signed-off-by: Chris Lalancette <clalance@redhat.com>

--- aeolusproject.org/lib/oz_support.rb
+++ aeolusproject.org/lib/oz_support.rb
@@ -3,6 +3,7 @@ module OzSupportHelper
     [
      { :name => "CentOS 5.x", :install => true, :customize => true, :icicle => true },
      { :name => "Fedora 7 - 15", :install => true, :customize => true, :icicle => true },
+     { :name => "OpenSUSE 11.x", :install => true, :customize => true, :icicle => true },
      # Had to be moved out of this array, and into the content page itself, so it could be visually styled correctly 
      #{ :name => "RHEL 5.x, 6.x", :install => true, :customize => true, :icicle => true },
     ]
@@ -13,10 +14,9 @@ module OzSupportHelper
      { :name => "CentOS 3.x, 4.x", :install => true, :customize => false, :icicle => false },
      { :name => "Debian 5, 6", :install => true, :customize => false, :icicle => false },
      { :name => "Fedora Core 1 - 6", :install => true, :customize => false, :icicle => false },
-     { :name => "OpenSUSE 11.x", :install => true, :customize => false, :icicle => true },
      { :name => "Red Hat Linux 7 - 9", :install => true, :customize => false },
      { :name => "RHEL 2.x, 3.x, 4.x <br /><em>(4.9 installs via repo not cdrom/ISO)</em>", :install => true, :customize => false, :icicle => false },
-     { :name => "Ubuntu 6.10 - 11.04", :install => true, :customize => false, :icicle => false },
+     { :name => "Ubuntu 6.06 - 11.04", :install => true, :customize => false, :icicle => false },
      # Had to be moved out of this array, and into the content page itself, so it could be visually styled correctly
      #{ :name => "Windows 2000, XP, 2003, 7, 2008", :install => true, :customize => false, :icicle => false },
     ]
