658a41db3d89f43c3e2a6ebab8a44bb06c8e8ab3
Justin Clift <jclift@redhat.com>
2011-10-27 13:11:41 +1100

    Bulk commit of initial updated content for Aeolus 0.3.0.

--- aeolusproject.org/lib/projects.rb
+++ aeolusproject.org/lib/projects.rb
@@ -3,7 +3,7 @@ module ProjectHelper
     [
       { :href => 'conductor.html', :menu => 'Conductor', :key => 'conductor', :description => 'Multiple cloud web UI' },
       { :href => 'oz.html', :menu => 'Oz', :key => 'oz', :description => 'Automated virtual machine installer' },
-      { :href => "imagefactory.html" , :menu => 'Image Factory', :key => 'imagefactory', :description => 'Daemon with a QMF interface for building cloud images' },
+      { :href => "imagefactory.html" , :menu => 'ImageFactory', :key => 'imagefactory', :description => 'Daemon with a QMF interface for building cloud images' },
       { :href => "imagewarehouse.html" , :menu => 'Image Warehouse', :key => 'imagewarehouse', :description => 'Daemon to move cloud images from cloud to cloud' },
       { :href => "audrey.html" , :menu => 'Audrey', :key => 'audrey', :description => 'A set of tools to do run-time configuration of cloud instances' }
     ]
